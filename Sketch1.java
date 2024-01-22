7deafb81f7964c18e5be9320fb26122ec8fbca25
Timchi12 <tsenkaiyo727@gmail.com>
2024-01-22 16:59:10 -0500

    added player movement, in process of fixing the game states, created global player variables, the player constructor function, and several other functions for the player, adding images and in process of adding stages in the game

--- Sketch1.java
+++ Sketch1.java
@@ -1,36 +1,45 @@
 import processing.core.PApplet;
-import processing.core.PFont;
 import processing.core.PImage;
 
-public class Sketch1 extends PApplet {
+/**
+ * A program thats creates a little game made by Timothy Chen and Ryan Chan of Mr. Fabroa's 2023/2024 ICS3U1 class
+ * @author: T. Chen & Ryan. Chan
+ */
 
+public class Sketch1 extends PApplet {
 
   // Global Variables
   int intMenuSelect; 
   float fltPlayerHp;
-  float fltHpBarPercent;
-  float fltHpBar = 250;
-  float fltTotalHitPoints = 10;
-  float fltCurrentHitPoints;
+  float fltHpBar = 300;
   boolean blnPlayerAlive;
-  
-  // Player Coord & Global Position cord
+  // Player Variables
   int intPlayerPosX;
   int intPlayerPosY; 
+  int intPlayerR; 
 
-  int intGlobalX;
-  int intGlobalY;
+  // Speed
+  int intPlayerS;
+    
+  // Variables to confirm player movement
+  boolean blnFaceR = true;
+  boolean blnFaceDown = true;
+  boolean blnIdle = true;
 
-  /** 
-   * Enumeration for the state of the game, menu vs gameply
-   * 
-   */
+  // Counts the ticks of the character moving to sync the animation
+  int intPlayerMoveTick = 0;
+  //  Number of ticks till the new step animation is animated
+  int intStepLength = 5;
+  // False is walk image 1, true is walk image 2
+  boolean blnStep = false;
+
+  // Enumeration for the state of the game, menu vs gameplay
 	private enum STATE{
     MENU,
     GAME,
     SELECTION,
     QUIT
-  };
+  }
 
   // Creates State variable to check what state the game is in based on
   private STATE State = STATE.MENU;
@@ -52,8 +61,9 @@ public class Sketch1 extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */ 
   public void settings() {
-	// put your size call here
+	  // put your size call here
     size(800, 800);
+
     // Loading Titlescreen Image
     imgTitleScreen = loadImage("titlebackground.jpg");
 
@@ -74,7 +84,10 @@ public class Sketch1 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    // Sets background colour
     background(0, 0, 0);
+
+    // Resizes the loaded images
     imgTitleScreen.resize(800, 800);
     imgPlayerUp1.resize(32, 32);
     imgPlayerUp2.resize(32, 32);
@@ -85,27 +98,23 @@ public class Sketch1 extends PApplet {
     imgPlayerDown1.resize(32, 32);
     imgPlayerDown2.resize(32, 32);
 
-    PFont mono;
-    mono = createFont("8bitoperator JVE", 50);
-    textFont(mono);
-    /*Font menuFont = new Font("8bitoperator JVE", Font.BOLD, 50);
-    setFont(M)
-    */
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    //  Selects the state of the game
     if ((intMenuSelect == 1) && (keyCode == ENTER)){
       background(255);
       State = STATE.GAME; 
     }
   
-  // Determines game state to be menu or game, changes screen depending on state, determines menu button select
-  if(State == STATE.MENU){
+  // Determines game state to be menu, selection, game or quit, changes screen depending on state, determines menu button select
+  if(State == STATE.MENU) {
   background(0);
   image(imgTitleScreen, 0, 0);
+
   if (keyPressed) {
     if (keyCode == UP) {
       intMenuSelect -= 1;
@@ -138,44 +147,135 @@ public class Sketch1 extends PApplet {
   fill(255, 0, 0);
   textSize(50);
   text("PLAY", 330, 245);
-  } else {
+    } else {
   if ( intMenuSelect == 2) { 
   fill(255, 0, 0);
   textSize(50);
   text("Character Selection", 180, 360);
-  } else {
+    } else {
   if (intMenuSelect == 3) { 
   fill(255, 0, 0);
   textSize(50);
   text("QUIT", 330, 475);
-  }
-  
+      }
+    }
   }
 
+    } else {    
+  if(State == STATE.GAME) {
+    Player();
+    Player(width / 2, height / 2, 32);
+    playerDirection();
+  /*while(blnPlayerAlive != false) {
+  }*/
+  healthBar();
+    }
   }
-    } else { 
-  if(State == STATE.GAME){
- /*  if (fltCurrentHitPoints > 0){
-    blnPlayerAlive = true;
-  }
-  if (blnPlayerAlive = true) {
-    for(fltCurrentHitPoints > 0; fltCurrentHitPoints <=10)
-  }
-  */
-  fltHpBarPercent = fltCurrentHitPoints / fltTotalHitPoints;
+}
+
+public void healthBar() {
+  // Creates Hp Bar
   strokeWeight(2);
   stroke(0);
   noFill();
-  rect(525, 25, 250, 25);
+  rect(50, 25, 250, 15);
 
   fill(255, 0, 0);
-  rect(525, 25, fltHpBar * fltHpBarPercent, 25); 
+  rect(50, 25, fltHpBar, 15);  
+  }
+  // define other methods down here. 
 
-      
+  public void Player() { 
+    intPlayerPosX = width / 2;
+    intPlayerPosX = height / 2; 
+    intPlayerR = 32;
+  }
+
+  public void Player(int intPosX, int intPosY, int intRadius) {
+    this.intPlayerPosX = intPosX;
+    this.intPlayerPosX = intPosX;
+    intPlayerR = intRadius; 
+  }
+
+  public void playerDirection() {
+    // Determines if player is facing right, if it is facing right, animate player moving right images
+    if (blnFaceR) 
+    {
+      if(intPlayerMoveTick > intStepLength) 
+      {
+        blnStep = !blnStep;
+        intPlayerMoveTick = 0;
+      }
+      if (blnStep) 
+      {
+        image(imgPlayerRight1, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+      else 
+      { 
+        image(imgPlayerRight2, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+    }
+    // Determines if player is facing left, if it is facing leftt, animate player moving left images
+    else if (!blnFaceR) 
+    {
+      if (intPlayerMoveTick > intStepLength)
+      {
+        blnStep = !blnStep;
+        intPlayerMoveTick = 0;
+      }
+
+      if (blnStep)
+      {
+        image(imgPlayerLeft1, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+      else 
+      {
+        image(imgPlayerLeft2, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+    }
+    // Determines if player is facing down, if it is facing down, animate player moving down images
+    else if (blnFaceDown) 
+    {
+      if (intPlayerMoveTick > intStepLength)
+      {
+        blnStep = !blnStep;
+        intPlayerMoveTick = 0;
+      }
+
+      if (blnStep)
+      {
+        image(imgPlayerDown1, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+      else 
+      {
+        image(imgPlayerDown2, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+    }
+    // Determines if player is facing up, if it is facing up, animate player moving down images
+    else if (!blnFaceDown) 
+    {
+      if (intPlayerMoveTick > intStepLength)
+      {
+        blnStep = !blnStep;
+        intPlayerMoveTick = 0;
+      }
+
+      if (blnStep)
+      {
+        image(imgPlayerUp1, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
+      else 
+      {
+        image(imgPlayerUp2, intPlayerPosX - intPlayerR, intPlayerPosY - intPlayerR);
+      }
     }
   }
-  
-  }
-  
-  // define other methods down here. 
 }
+
+
+
+
+
+
+// TotalHp, sets the frame
+// If damage is taken, hp goes lower
