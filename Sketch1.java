8b8af805c6506a7e58485ce3937d0e1e21f55183
Timchi12 <tsenkaiyo727@gmail.com>
2024-01-23 07:30:00 -0500

    select screen images and varaivles added, health bar being tweaked, stage number has been added that changes according to clear condition.  Implementing attack animation and making the sprites

--- Sketch1.java
+++ Sketch1.java
@@ -10,9 +10,14 @@ public class Sketch1 extends PApplet {
 
   // Global Variables
   int intMenuSelect; 
-  float fltPlayerHp;
+  int intPlayerHp = 10;
   float fltHpBar = 300;
   boolean blnPlayerAlive;
+  // Defining variables for weapon animation and selection
+  int intWeaponSelect;
+  int intWeaponYSpeed = 1;
+  int intSwordY = 350;
+  int intWandY = 390;
   // Player Variables
   int intPlayerPosX;
   int intPlayerPosY; 
@@ -57,6 +62,14 @@ public class Sketch1 extends PApplet {
 
   // Title Background
   PImage imgTitleScreen;
+
+  // Selection menu objects
+  PImage imgSelectScreen;
+  PImage imgSwordS1;
+  PImage imgWandS2;
+  PImage imgSwordSelected;
+  PImage imgWandSelected;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */ 
@@ -66,6 +79,14 @@ public class Sketch1 extends PApplet {
 
     // Loading Titlescreen Image
     imgTitleScreen = loadImage("titlebackground.jpg");
+
+    // Loading Selection menu object Images
+    imgSelectScreen = loadImage("selectionbg.jpg");
+    imgSwordS1 = loadImage("swords1.png");
+    imgWandS2 = loadImage("wands2.png");
+    imgSwordSelected = loadImage("swordSelected.png");
+    imgWandSelected = loadImage("wandSelected.png");
+
 
     // Loading Player Images
     imgPlayerUp1 = loadImage("player_up_1.png");
@@ -86,9 +107,14 @@ public class Sketch1 extends PApplet {
   public void setup() {
     // Sets background colour
     background(0, 0, 0);
-
+    frameRate(60);
     // Resizes the loaded images
+    imgSelectScreen.resize(800, 800);
     imgTitleScreen.resize(800, 800);
+    imgSwordS1.resize(256, 256);
+    imgWandS2.resize(128, 128);
+    imgSwordSelected.resize(256, 256);
+    imgWandSelected.resize(128, 128);
     imgPlayerUp1.resize(32, 32);
     imgPlayerUp2.resize(32, 32);
     imgPlayerLeft1.resize(32, 32);
@@ -108,6 +134,15 @@ public class Sketch1 extends PApplet {
     if ((intMenuSelect == 1) && (keyCode == ENTER)){
       background(255);
       State = STATE.GAME; 
+    } 
+    else if ((intMenuSelect == 2) && (keyCode == ENTER)){
+      State = STATE.SELECTION; 
+    }
+    else if ((intMenuSelect == 3) && (keyCode == ENTER)) {
+      State = STATE.QUIT;
+      if (State == STATE.QUIT) {
+        exit();
+      }
     }
   
   // Determines game state to be menu, selection, game or quit, changes screen depending on state, determines menu button select
@@ -137,53 +172,116 @@ public class Sketch1 extends PApplet {
 
   fill(0);
   textSize(50);
-  text("Character Selection", 180, 360);
+  text("Weapon Selection", 180, 360);
   
   fill(0);
   textSize(50);
   text("QUIT", 330, 475);
 
+
   if (intMenuSelect == 1) {
+
   fill(255, 0, 0);
   textSize(50);
   text("PLAY", 330, 245);
+
     } else {
   if ( intMenuSelect == 2) { 
+
   fill(255, 0, 0);
   textSize(50);
-  text("Character Selection", 180, 360);
+  text("Weapon Selection", 180, 360);
+
     } else {
-  if (intMenuSelect == 3) { 
+  if (intMenuSelect == 3) {
+
   fill(255, 0, 0);
   textSize(50);
   text("QUIT", 330, 475);
-      }
-    }
-  }
-
-    } else {    
-  if(State == STATE.GAME) {
-    Player();
-    Player(width / 2, height / 2, 32);
-    playerDirection();
-  /*while(blnPlayerAlive != false) {
-  }*/
-  healthBar();
-    }
   }
 }
 
-public void healthBar() {
-  // Creates Hp Bar
-  strokeWeight(2);
-  stroke(0);
-  noFill();
-  rect(50, 25, 250, 15);
+}
 
-  fill(255, 0, 0);
-  rect(50, 25, fltHpBar, 15);  
+ } 
+ else if(State == STATE.GAME) {
+    Player();
+    Player(width / 2, height / 2, 32);
+    playerDirection();
+    healthBar();
   }
+  else if (State == STATE.SELECTION) {
+    image(imgSelectScreen, 0, 0, 800, 800); 
+    image(imgSwordS1, 200, intSwordY);
+    image(imgWandS2, 430, intWandY);
+
+    // If statement to determine weapon select and giving the weapon a numerical value
+    if (keyPressed) {
+      
+      if (keyCode == LEFT) {
+
+        intWeaponSelect -= 1;
+        delay(60);
+
+        if (intMenuSelect < 1) {
+          intMenuSelect = 1;
+        }
+      } else if (keyCode == RIGHT) {
+
+        intWeaponSelect += 1;
+        delay(60);
+        
+        if (intMenuSelect > 2) {
+          intMenuSelect = 2;
+        }
+      }
+      // If statement that will animate the weapon
+      if (intWeaponSelect == 1) {
+        image(imgSwordSelected, 200, intSwordY);
+      }
+      else if (intWeaponSelect == 2) {
+        image(imgWandSelected, 430, intWandY);
+        }
+      }
+  }
+ }
   // define other methods down here. 
+  public void healthBar() {
+    // Creates Hp Bar
+    strokeWeight(2);
+    stroke(0);
+    noFill();
+    rect(50, 25, 300, 15);
+  
+    fill(255, 0, 0);
+    rect(50, 25, fltHpBar, 15);  
+    }
+
+  public void stageSelect() {
+
+    int intStageNumber = 0;
+
+    boolean blnClearCondition;
+    // Sets the condition for player death
+    if (intPlayerHp == 0) {
+      blnPlayerAlive = false;
+    }
+    // Sets the clear condition of a stage to be based off the player alive status and stage changes
+    if(blnPlayerAlive == false) {
+
+      blnClearCondition = true; 
+      if ((blnClearCondition == true) && (intStageNumber == 0)) {
+        intStageNumber += 1;
+      }
+
+    }
+     if (intStageNumber == 0) {
+      // tutorial stuff
+     }
+     else if (intStageNumber == 2) {
+      // Boss level
+     }
+    }
 
   public void Player() { 
     intPlayerPosX = width / 2;
